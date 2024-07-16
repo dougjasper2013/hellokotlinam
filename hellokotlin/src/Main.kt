b492545317934efb005909f0fbfd11bce3c0f50c
Douglas Jasper <doug.jasper@trios.com>
2024-07-16 10:23:15 -0400

    End of Chapter 5

--- hellokotlin/src/Main.kt
+++ hellokotlin/src/Main.kt
@@ -37,6 +37,17 @@ fun main() {
         }
     }
 
+    val count = 10
+
+    var total: Int = 0
+    for (i in 1..count) {
+        total += i // equivalent to total = total + i
+        println("total = $total" )
+    }
+
+    println("Final total = $total" )
+
+
 
 
 
