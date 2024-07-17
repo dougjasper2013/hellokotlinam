17ac178fb5dd7308f5428ee29495fe6f70447098
Douglas Jasper <doug.jasper@trios.com>
2024-07-17 09:35:23 -0400

    End of Chapter 8

--- hellokotlin/src/Main.kt
+++ hellokotlin/src/Main.kt
@@ -102,6 +102,39 @@ fun main() {
 
     println(mustHaveResult)
 
+    val innerPlanets = listOf("Mercury", "Venus", "Earth", "Mars")
+
+    val testList = listOf("Mercury", 5, 6.0, "Mars")
+
+    val testArray = arrayOf("Mercury", 5, 6.0, "Mars")
+
+    val players = mutableListOf("Alice", "Bob", "Cindy", "Dan")
+
+    println(players.isEmpty())
+// > false
+    var currentPlayer = players.first()
+    println(currentPlayer) // > Alice
+
+    currentPlayer = players[1]
+    println(currentPlayer) // > Alice
+
+    println(players)
+
+    players.forEach { player ->
+        println(player)
+    }
+
+    val upcomingPlayersSlice = players.slice(1..2)
+    println(upcomingPlayersSlice.joinToString()) // > Bob, Cindy
+
+    players.add("Eli")
+    players.forEach { player ->
+        println(player)
+    }
+
+
+
+
 
 }
 
